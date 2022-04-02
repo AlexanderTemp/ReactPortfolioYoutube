89c937748ee684a8cd3a570079de241fa2af9fb5
AlexanderTemp <aaalex2025@gmail.com>
2022-04-01 21:18:44 -0400

    header terminado v_1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hola Yo soy</h5>
+        <h1>Alexander Nina</h1>
+        <h5 className="text-light">Aprendiz de React</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className="me">
+          <img src={ME} alt="me"/>
+        </div>
+        <a href='#contact' className="scroll__down">Bajar</a>
+      </div>
+    </header>
   )
 }
 
